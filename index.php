e7f15f1bb4f1d78b1cfb394e092ffcee45f4dd9b
luthfi <lardinatafirman@gmail.com>
2024-02-12 00:01:54 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,252 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Luthfi Ardinata</title>
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body class="bg-light">
+
+    <div class="container">
+
+        <!--NAVBAR-->
+        <div class="container py-3">
+            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">Luthfi Ardinata Firman</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Link</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+
+
+        <!--CONTAINER1-->
+        <div class="card mt-3 text-light shadow-lg p-3 mb-5" style="width:auto;background-image:url('assets/bgprofils.png');background-size:cover">
+            <center>
+                <img src="assets/profilpicture.jpg" class="card-img-top round rounded-3 border border-light border-3 mt-5 mb-3" style="width:10em">
+                <div class="card-body">
+                    <p class="card-text">
+                    <h2 style="text-shadow: 0 0 10px #000, 0 0 10px #000, 0 0 10px #000;">Luthfi Ardinata Firman</h2><br>
+                    <h6 style="text-shadow: 0 0 10px #000, 0 0 10px #000, 0 0 10px #000;">Web Developer & Desainer Grafis</h6>
+                    <img src="" alt="">
+                    </p>
+                    <button type="button" class="btn btn-info text-light">Hubungi Saya</button>
+                </div>
+            </center>
+        </div>
+
+        <!--CONTAINER2-->
+        <div class="container mt-4 round rounded-3 shadow-lg p-3 mb-5 bg-body-tertiary">
+            <div class="row ">
+                <div class="col-md-6 p-5 ">
+                    <h2>Tentang Saya</h2><br>
+                    <p>Hallo! Saya Luthfi Ardinata. Saya tertarik dengan pembuatan website, terutama di bagian front-end dan
+                        ini adalah website saya.<br>
+                        Selamat datang.</p>
+                </div>
+                <div class="col-md-6 p-5 ">
+                    <h2>Bio</h2>
+                    <div class="row">
+                        <div class="col-3  ">
+                            <div class="row">
+                                <strong>Umur</strong>
+                            </div>
+                            <div class="row">
+                                <strong>Email</strong>
+                            </div>
+                            <div class="row">
+                                <strong>Phone</strong>
+                            </div>
+                            <div class="row">
+                                <strong>Alamat</strong>
+                            </div>
+                        </div>
+                        <div class="col-9 ">
+                            <div class="row">
+                                19
+                            </div>
+                            <div class="row">
+                                lardinatafirman@gmail.com
+                            </div>
+                            <div class="row">
+                                +62 895 0987 8729
+                            </div>
+                            <div class="row">
+                                Jl. Raya Krapyak
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!--CONTAINER3-->
+        <div class="container mt-4 round rounded-3 shadow-lg p-3 mb-5 bg-body-tertiary">
+            <div class="row pt-5 ps-5">
+                <h2>Keahlian</h2>
+                <div class="col-md-6 p-5 ">
+
+                    <h6><strong>HTML</strong></h6>
+                    <div class="progress " role="progressbar" aria-label="Info example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
+                        <div class="progress-bar bg-info text-light aos-init aos-animate" data-aos="fade-right" style="width: 75%">ADVANCED</div>
+                    </div> <br>
+
+                    <h6><strong>JavaScript</strong></h6>
+                    <div class="progress " role="progressbar" aria-label="Info example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
+                        <div class="progress-bar bg-info text-light aos-init aos-animate" data-aos="fade-right" style="width: 50%">BEGINNER</div>
+                    </div><br>
+
+                    <h6><strong>CSS</strong></h6>
+                    <div class="progress " role="progressbar" aria-label="Info example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
+                        <div class="progress-bar bg-info text-light aos-init aos-animate" data-aos="fade-right" style="width: 75%">ADVANCED</div>
+                    </div><br>
+
+                    <h6><strong>PHP</strong></h6>
+                    <div class="progress " role="progressbar" aria-label="Info example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
+                        <div class="progress-bar bg-info text-light aos-init aos-animate" data-aos="fade-right" style="width: 50%">BEGINNER</div>
+                    </div>
+
+
+                </div>
+                <div class="col-md-6 p-5 ">
+
+                    <h6><strong>C++</strong></h6>
+                    <div class="progress " role="progressbar" aria-label="Success example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
+                        <div class="progress-bar bg-success text-light aos-init aos-animate" data-aos="fade-right" style="width: 75%">ADVANCED</div>
+                    </div> <br>
+
+                    <h6><strong>Python</strong></h6>
+                    <div class="progress " role="progressbar" aria-label="Success example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
+                        <div class="progress-bar bg-success text-light aos-init aos-animate" data-aos="fade-right" style="width: 75%">ADVANCED</div>
+                    </div> <br>
+
+                    <h6><strong>MySQL</strong></h6>
+                    <div class="progress " role="progressbar" aria-label="Success example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
+                        <div class="progress-bar bg-success text-light aos-init aos-animate" data-aos="fade-right" style="width: 50%">BEGINNER</div>
+                    </div> <br>
+
+                    <h6><strong>Cisco</strong></h6>
+                    <div class="progress " role="progressbar" aria-label="Success example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
+                        <div class="progress-bar bg-success text-light aos-init aos-animate" data-aos="fade-right" style="width: 75%">ADVANCED</div>
+                    </div> <br>
+
+                </div>
+            </div>
+        </div>
+
+
+        <!--CONTAINER4-->
+        <div class="container mt-4 round rounded-3 shadow-lg p-3 mb-5 bg-body-tertiary">
+            <div class="row pt-5 ps-5">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h2>Pengalaman</h2><br>
+                        <ul class="timeline my-4">
+                            <li>
+                                <h5>Asisten Lab Jurusan Informatika UPN Veteran Yogyakarta</h5>
+                                <p>Menjadi pengajar di laboratorium informatika UPN Veteran Yogyakarta.</p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!--CONTAINER5-->
+        <div class="container mt-4 round rounded-3 shadow-lg p-3 mb-5 bg-body-tertiary">
+            <div class="row pt-5 ps-5">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h2>Kepanitiaan</h2><br>
+                        <ul class="timeline my-4">
+                            <li>
+                                <h5>DISKU-SI (Perlengkapan)</h5>
+                                <p>Mempersiapkan peralatan serta dekorasi untuk acara DISKU-SI.</p>
+                            </li>
+                            <li>
+                                <h5>CB & Orca (PDD)</h5>
+                                <p>Menjadi operator zoom meeting pada acara CB.
+                                    <br>Mendokumentasikan serangkaian kegiatan pada acara Orca.
+                                </p>
+                            </li>
+                            <li>
+                                <h5>CWSI (PDD)</h5>
+                                <p>Mendokumentasikan serangkaian kegiatan pada acara CWSI.</p>
+                            </li>
+                            <li>
+                                <h5>Positive Cup (Acara)</h5>
+                                <p>Merangkai jalannya acara pertandingan basket pada event Positive Cup.</p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!--CONTAINER5-->
+        <div class="container mt-4 round rounded-3 shadow-lg p-3 mb-5 bg-body-tertiary">
+            <div class="row pt-5 ps-5">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h2>Kontak</h2><br>
+                        <p style="margin-bottom: 2em;">Phone : +62 895 0987 8729 <br>
+                            Email : lardinatafirman@gmail.com <br>
+                        </p>
+                        <form action="POST">
+                            <div class="mb-3">
+                                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Nama">
+                            </div>
+                            <div class="mb-3">
+                                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Email">
+                            </div>
+                            <div class="mb-3">
+                                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Pesan"></textarea>
+                            </div>
+                            <div class="mb-3">
+                                <input type="submit" class="btn btn-info text-light" style="width:100%;">
+                            </div>
+                        </form>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="ratio ratio-4x3">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d988.392859475366!2d110.41320836956811!3d-7.72904329951806!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a594333d7c3dd%3A0xa68074fe4b6aff96!2sJl.%20Raya%20Krapyak%2044%2C%20Bakungan%2C%20Wedomartani%2C%20Kec.%20Ngemplak%2C%20Kabupaten%20Sleman%2C%20Daerah%20Istimewa%20Yogyakarta%2055584!5e0!3m2!1sen!2sid!4v1707667919348!5m2!1sen!2sid" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <footer>
+            <center>
+                <p class="p-4" style="color: #8f8f8f;">Created by Luthfi Ardinata.</p>
+            </center>
+        </footer>
+
+    </div>
+
+
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script>
+        AOS.init();
+    </script>
+</body>
+
+</html>
